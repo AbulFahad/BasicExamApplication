8a8f0c5fdd017b21e07dcdea150d8daa44275d37
Abul Fahad <abulfahad786@gmail.com>
2024-05-19 20:07:48 +0530

    C1 : Adding all the services to master

---
+++ ReportSevice/ReportSevice/src/main/java/com/ReportSevice/ReportSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.ReportSevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportSeviceApplication.class, args);
+	}
+
+}
